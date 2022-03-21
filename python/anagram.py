a41eb67dde087adcaa0dd8e5388e62519df6e5e5
Christoffer Ridderland <chrissetheboss@gmail.com>
2022-03-22 00:03:59 +0100

    python katas

---
+++ python/anagram.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/python
+def letterCounter(word):
+    letters = {}
+    for letter in word:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
+
+def anagrams(word, words):
+    anag = []
+    wordMap = letterCounter(word)
+    for w in words:
+        if wordMap == letterCounter(w):
+            anag.append(w)
+    return anag
---
+++ python/secondsToReadable.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/52685f7382004e774f0001f7/train/python
+def make_readable(seconds):
+    h = seconds // 3600
+    m = (seconds - h * 3600) // 60
+    s = seconds - 60 * (m + 60 * h)
+    time = ""
+    for t in [h, m, s]:
+        if t < 10:
+            time += "0"
+        time += "{t}:".format(t = t)
+    return time[:-1]
